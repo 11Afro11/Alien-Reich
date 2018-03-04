b1e6b9f317a107413fb7aa9bb117dedc031d62ad
Unknown <scmarquez@correo.ugr.es>
2018-03-04 17:14:12 +0100

    Esto tiene algunos scripts, el de moverse, el de cambiar el ratón, el de la cámara.

---
+++ Assets/scripts/GameSettings/CustomCursor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomCursor : MonoBehaviour {
+    public Texture2D imagen; //Imagen asignada al cursor
+    private CursorMode cursorMode = CursorMode.Auto; //Allow this cursor to render as a hardware cursor on supported platforms, or force software cursor
+    private Vector2 hotSpot = Vector2.zero; //The offset from the top left of the texture to use as the target point (must be within the bounds of the cursor).
+    private void Start()
+    {
+        Cursor.SetCursor(imagen, hotSpot, cursorMode);
+    }
+    //Al entrar el ratón en pantalla
+    private void OnMouseEnter() {
+        Cursor.SetCursor(imagen, hotSpot, cursorMode);
+	}
+    //Al salir el curosor de la pantalla
+    private void OnMouseExit(){
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
+    }
+
+}
---
+++ Assets/scripts/Personajes/MainChracter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainChracter : MonoBehaviour {
+    
+    public KeyCode forward_key;
+    public KeyCode backward_key;
+    public KeyCode left_key;
+    public KeyCode right_key;
+    public int max_live;
+    public float mov_speed;
+    public GameObject camera;
+    public GameObject cam_position;
+
+    [Range(0.0f,1.0f)]//Decorador para poner tope al valor de la variable elasticidad
+    public float elasticidad;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {  
+        
+        //Mueve al personaje según la tecla que se pulse
+        if (Input.GetKey(forward_key)){
+          
+            transform.Translate(0, 0, mov_speed * Time.deltaTime);
+            
+        }
+        else if (Input.GetKey(backward_key)){
+            transform.Translate(0, 0, -mov_speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(left_key)){
+            transform.Translate(new Vector3(-1, 0, 0)*Time.deltaTime*mov_speed);
+        }
+        else if (Input.GetKey(right_key)) {
+            transform.Translate(mov_speed * Time.deltaTime, 0, 0);
+        }
+        //Elasticidad de la camara
+        camera.transform.position = Vector3.Lerp(camera.transform.position, cam_position.transform.position, elasticidad);
+	}
+}
